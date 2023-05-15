0a9b63b0842a77ced1e92a1893b2367807b32867
MassaHex <105466578+MassaHex@users.noreply.github.com>
2023-05-15 19:07:44 -0400

    Update InstantAI.js

--- InstantAI.js
+++ InstantAI.js
@@ -1,7 +1,7 @@
 javascript: (function() {
     var iframe = document.createElement('iframe');
     iframe.src = "https://deepai.org/chat";
-    iframe.sandbox = 'allow-forms allow-scripts';
+    iframe.sandbox = 'allow-forms allow-scripts allow-same-origin';
     iframe.style.position = "fixed";
     iframe.style.top = "-500px";
     iframe.style.left = "calc(50% - 500px)";
